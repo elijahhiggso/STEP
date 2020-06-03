047719150477f04e66e02d9801017d84eb3b4bc7
Elijah <elijahhiggso@gmail.com>
2020-06-03 21:04:46 +0000

    Add fetch for greeting

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,9 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+// Fetch Greeting from DataServlet.java
+async function getMessage(){
+    const response = await fetch('/data');
+    const greeting = await response.text();
+    document.getElementById('greeting-container').innerText = greeting;
+}
